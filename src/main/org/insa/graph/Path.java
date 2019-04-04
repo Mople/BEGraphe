5dbd172fab87b2a403d03b8377b47815450e3c51
M. Dauriac Alexandre <aldauria@insa-20047>
2019-04-04 10:29:14 +0200

    Méthode getTravelTime terminée.

--- src/main/org/insa/graph/Path.java
+++ src/main/org/insa/graph/Path.java
@@ -228,7 +228,7 @@ public class Path {
      * @return Time (in seconds) required to travel this path at the given speed (in
      *         kilometers-per-hour).
      * 
-     * @deprecated Need to be implemented.
+     * 
      */
     public double getTravelTime(double speed) {
     	return getLength() * 3600.0 / (speed * 1000.0);
