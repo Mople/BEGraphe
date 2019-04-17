941201a541d83c46ab67186510e5e4b69d2f0903
M. Dauriac Alexandre <aldauria@insa-08134>
2019-04-17 18:13:36 +0200

    Debut de l'algo de Dijkstra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,10 @@
 package org.insa.algo.shortestpath;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.insa.graph.*;
+import org.insa.algo.utils.*;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,7 +15,53 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        Graph graph = data.getGraph();
+
+        //Initialisation
+        final int nbNodes = graph.size();
+        List<Node> list_node=graph.getNodes();
+        Label[] tabLabels = new Label[nbNodes];
+        
+        
+        for (int i=0; i<nbNodes ;i++) {
+        	tabLabels[i] = new Label(list_node.get(i));
+        }
+        tabLabels[data.getOrigin().getId()].cout=0;
+        BinaryHeap<Node> Tas = new BinaryHeap<Node>();
+        Tas.insert(data.getOrigin());
+        
+        //Iteration
+        boolean allmarked=false;
+        while (!allmarked) {
+        	Node x = Tas.findMin();
+        	tabLabels[x.getId()].marque=true;
+        	
+        	List<Arc> Suivants = x.getSuccessors();
+        	
+        	for (int i=0;i<Suivants.size();i++) {
+        		
+        	}
+        	
+        	
+        	
+        	
+        	//Check sommets non marqués
+        	allmarked=true;
+        	for (int i=0; allmarked&&i<nbNodes;i++) {
+        		if (!tabLabels[i].marque) {
+        			allmarked=false;
+        		}
+        	}
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
         return solution;
     }
 
